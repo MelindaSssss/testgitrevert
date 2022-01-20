a9740555c3e63771105988430a72dbbbe9d98409
施艳 <shiyan@datatom.com>
2022-01-20 17:31:30 +0800

    develop上添加了

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 const App = () => {
   console.info("HHHHHHHHHHHH");
   console.info("CCCCCCCCCCC");
+
+  console.info("在main上添加了一个tag");
+  console.info("在develop上添加了");
   return <div>测试首页数据</div>;
 };
 
