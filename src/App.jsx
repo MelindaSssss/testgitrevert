77fcfd1ffe0ea57f17d2199c99efe20e2945f193
施艳 <shiyan@datatom.com>
2022-01-20 17:33:48 +0800

    revert a 就是到a之前的提交状态

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 const App = () => {
   console.info("HHHHHHHHHHHH");
   console.info("CCCCCCCCCCC");
+<<<<<<< HEAD
 
   console.info("在main上添加了一个tag");
   console.info("在develop上添加了");
+=======
+>>>>>>> parent of 6d138ad (在main上添加tag)
   return <div>测试首页数据</div>;
 };
 
